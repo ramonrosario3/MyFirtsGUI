5789ad4a2b34a57ad42d523225e6ad99fd1cf598
ramon <ramon@10.32.13.178>
2018-03-20 17:27:01 -0400

    added carComponent to draw a car

--- MyFirstGUI/src/Main.java
+++ MyFirstGUI/src/Main.java
@@ -1,14 +1,22 @@
 import javax.swing.JFrame;
 
 public class Main {
-	public static void main (String[] args) {
-		JFrame ventana = new JFrame();
-		ventana.setSize(800, 600);
-		ventana.setTitle("My First GUI!!!");
-		ventana.setVisible(true);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	public static void main(String[] args) {
 		
+		JFrame mainFrame = new JFrame();
+		
+		mainFrame.setSize(800, 600);
+		
+		mainFrame.setTitle("My First GUI!!!");
+		
+		MyComponent carComponent = new MyComponent();
+		
+		mainFrame.add(carComponent);
+		
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainFrame.setVisible(true);
 	}
 	
-
 }
