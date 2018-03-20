81298d8eed320b5d1b135ad726fe0d8fc5e6e209
ramon <ramon@10.32.13.178>
2018-03-20 11:48:15 -0400

    initial commit

---
+++ MyFirstGUI/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main (String[] args) {
+		JFrame ventana = new JFrame();
+		ventana.setSize(800, 600);
+		ventana.setTitle("My First GUI!!!");
+		ventana.setVisible(true);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+
+}
